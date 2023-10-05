85e6ea3610185ccdeaadbda466ea16442903039c
Guillermo Sotelo <guillermo.sotelo@volvocars.com>
2023-10-05 16:31:29 +0200

    enhanced fonts

--- src/components/CustomInput.js
+++ src/components/CustomInput.js
@@ -10,7 +10,7 @@ const CustomInput = ({ customInput, setCustomInput, isMobile }) => {
         onChange={(e) => setCustomInput(e.target.value)}
         placeholder={`Custom input`}
         style={{ width: isMobile ? '91vw' : '97%' }}
-        className={`${isMobile ? 'mx-3' : 'w-full mx-4'} outline-0 bg-[#121d23] p-4 rounded-md text-white font-normal text-sm overflow-y-auto`}
+        className={`${isMobile ? 'mx-3' : 'w-full mx-4'} outline-0 bg-[#121d23] p-4 rounded-md text-white font-normal text-m overflow-y-auto`}
       ></textarea>
     </div>
   );
--- src/components/OutputWindow.js
+++ src/components/OutputWindow.js
@@ -7,13 +7,13 @@ const OutputWindow = ({ outputDetails, isMobile }) => {
     if (statusId === 6) {
       // compilation error
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+        <pre className="px-2 py-1 font-normal text-m text-red-500">
           {atob(outputDetails?.compile_output)}
         </pre>
       );
     } else if (statusId === 3) {
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-green-500">
+        <pre className="px-2 py-1 font-normal text-m text-green-500">
           {atob(outputDetails.stdout) !== null
             ? `${atob(outputDetails.stdout)}`
             : null}
@@ -21,13 +21,13 @@ const OutputWindow = ({ outputDetails, isMobile }) => {
       );
     } else if (statusId === 5) {
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+        <pre className="px-2 py-1 font-normal text-m text-red-500">
           {`Time Limit Exceeded`}
         </pre>
       );
     } else {
       return (
-        <pre className="px-2 py-1 font-normal text-xs text-red-500">
+        <pre className="px-2 py-1 font-normal text-m text-red-500">
           {atob(outputDetails?.stderr)}
         </pre>
       );
@@ -38,7 +38,7 @@ const OutputWindow = ({ outputDetails, isMobile }) => {
       <h1 className="font-bold text-xl bg-clip-text bg-gradient-to-r my-2">
         Output
       </h1>
-      <div className={`${isMobile ? '' : 'w-full'} h-56 bg-[#121d23] p-4 rounded-md text-white font-normal text-sm overflow-y-auto`}>
+      <div className={`${isMobile ? '' : 'w-full'} h-56 bg-[#121d23] p-4 rounded-md text-white font-normal text-m overflow-y-auto`}>
         {outputDetails ? <>{getOutput()}</> : null}
       </div>
     </div>
